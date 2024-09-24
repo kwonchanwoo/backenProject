c582a396a46de7d2f55092ef623a2021db81140d
hoyeong <hoyeong96@naver.com>
2024-09-24 22:24:52 +0900

    FileCustomRepo, FileCustomRepoImpl 추가

--- src/main/java/com/example/module/api/file/dto/response/ResponseFileDto.java
+++ src/main/java/com/example/module/api/file/dto/response/ResponseFileDto.java
@@ -1,6 +1,7 @@
 package com.example.module.api.file.dto.response;
 
 import com.example.module.entity.File;
+import com.example.module.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseFileDto {
-    private String name;
+    private String fileName;
     private String description;
+    private String memberName;
     private long size;
 
     public ResponseFileDto(File file) {
-        this.name = file.getName();
+        this.fileName = file.getName();
         this.description = file.getDescription();
         this.size = file.getSize();
-
-        // member key값 어디서 넣누
+        this.memberName = file.getMember().getName();
     }
 
 }
